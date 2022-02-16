b1e4b5ac0d3af31d728cf3c83a13dd040dde1eff
Satya Prakash <80380695+Sa-tya@users.noreply.github.com>
2022-02-16 14:38:41 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,18 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-// import App from './d20_food';
-// import App from './Day_27/app'
-// import App from './Day_28/app'
-// import App from './components/d21_edit';
-// import App from './chess_board';
-// import App from './Context_hook/provider';
-// import reportWebVitals from './reportWebVitals';
-// import App from './Day_23/Home'
-// import App from  './Day_24/router'
-// import App from './LifeCycle/Intro'
-// import App from './memesGenerator/MemeGenerator';
-import App from './weatherAPI/app';
+
+import App from './app';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -20,7 +10,3 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-// reportWebVitals();
